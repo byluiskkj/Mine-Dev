a92a8dca11649a58eb0b5aad3c939a3726fff668
samuelrosariodev <samuelrosario.dev@gmail.com>
2024-11-18 12:30:43 -0300

    add cart and login

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,133 +1,118 @@
-import React from 'react'
-import './Home.css'
+import React, { useState, useEffect } from "react";
+import "./Home.css";
+import axios from "axios";
 
 const Home = () => {
-    return (
-      
-      <header>
+	const [produtos, setProdutos] = useState([]);
+	const [carrinho, setCarrinho] = useState([]);
 
-        <div class="navbar">
-          <div class="conteúdo interno do cabeçalho">
-            <h1 class="logo">Mine <span>Dev</span></h1>
-            <nav>
-              <ul>
-                <li><a href="">Sobre nosso site!</a></li>
-                <li><a href="">Contato!</a></li>
-                <li><a href="">Somos confiaveis!?</a></li>
-              </ul>
-            </nav>
-            <div class="nav-img-container">
-              <img src="/src/assets/cart.png" alt="IMAGEM DO CART!" />
-              <img class="" src="./src/assets/menu.png" alt="IMAGEM DO MENU!" />
-            </div>
-          </div>
-        </div>
-        <div class="container">
-        <div class="card card1">
-            <img src="./src/assets/imagem(Capa Optifine).png" alt="" />
-            <h1>Minecraft Full Acesso</h1>
-            <p> NOVIDADE: 90% de chance de ter a Capa Creeper (Aniversário 15
-                anos), Capa Twitch (Purple Heart) & Capa Tiktok (Follower’s)
-                (Capas do Aniversário de 15 anos)
-                <br />
-                Minecraft Java Edition e Bedrock Edition
-                (Altera E-mail, Senha, Nick e Skin) Ativação diretamente em seu email
-                <br />
-                Acesso permanente e completo à todas as funções
-                (Alteração de dados e a todos os servidores originais) 
-                <br />
-                Acesso oficial e privado a versão Java & Bedrock EXCLUSIVO: 50% de chance
-                de ter a Migrator Cape, Vanilla Cape, Cherry Blossom Cape e/ou MCC
-                15th Year Cape (Capas oficiais)
-                <br />
-                Chance de ter Rank no Hypixel (VIP, VIP+, MVP, MVP+)
-                Chance de ter Cosméticos no Lunar ou Badlion
-            </p>
-                <button class="btn-1"> Comprar - R$ 70,00 </button>
-        </div>
-        <div class="card card2">
-            <img src="./src/assets/Imagem(Contas Full Acesso).png" alt="" />
-            <h1>blablabla</h1>
-            <p> NOVIDADE: 90% de chance de ter a Capa Creeper (Aniversário 15
-                anos), Capa Twitch (Purple Heart) & Capa Tiktok (Follower’s)
-                (Capas do Aniversário de 15 anos)
-                <br />
-                Minecraft Java Edition e Bedrock
-                Edition (Altera E-mail, Senha, Nick e Skin)
-                <br />
-                Ativação diretamente em seu e-mail
-                <br />
-                Acesso permanente e completo à todas as funções
-                (Alteração de dados e a todos os servidores originais) 
-                <br />
-                Acesso oficial e privado a versão Java & Bedrock EXCLUSIVO: 50% de chance
-                de ter a Migrator Cape, Vanilla Cape, Cherry Blossom Cape e/ou MCC
-                15th Year Cape (Capas oficiais)
-                <br />
-                Chance de ter Rank no Hypixel (VIP, VIP+, MVP, MVP+)
-                Chance de ter Cosméticos no Lunar ou Badlion
-                </p>
-            <button class="btn-2"> Comprar - R$ 70,00 </button>
+	useEffect(() => {
+		async function fetchProducts() {
+			try {
+				const response = await axios.get("http://localhost:5000/produtos");
+				setProdutos(response.data); // Atualiza o estado com os produtos
+			} catch (error) {
+				console.error("Erro ao buscar produtos:", error);
+			}
+		}
 
-        </div>
-        <div class="card card3">
-            <img src="./src/assets/imagem(Contas semi-acesso).png" alt="" />
-            <h1>blablabla</h1>
-            <p> NOVIDADE: 90% de chance de ter a Capa Creeper (Aniversário 15
-                anos), Capa Twitch (Purple Heart) & Capa Tiktok (Follower’s)
-                (Capas do Aniversário de 15 anos)
-                <br />
-                Minecraft Java Edition e Bedrock
-                Edition (Altera E-mail, Senha, Nick e Skin)
-                <br />
-                Ativação diretamente em seu e-mail
-                <br />
-                Acesso permanente e completo à todas as funções
-                (Alteração de dados e a todos os servidores originais) 
-                <br />
-                Acesso oficial e privado a versão Java & Bedrock EXCLUSIVO: 50% de chance
-                de ter a Migrator Cape, Vanilla Cape, Cherry Blossom Cape e/ou MCC
-                15th Year Cape (Capas oficiais)
-                <br />
-                Chance de ter Rank no Hypixel (VIP, VIP+, MVP, MVP+)
-                Chance de ter Cosméticos no Lunar ou Badlion
-                </p>
-            <button class="btn-3"> Comprar - R$ 70,00 </button>
+		fetchProducts();
 
-        </div>
-    </div>
+		const carrinhoSalvo = JSON.parse(localStorage.getItem("carrinho")) || [];
+		setCarrinho(carrinhoSalvo);
+	}, []); // O array vazio [] garante que o useEffect será executado apenas na montagem
 
-      <footer>
-      <div class="footer-container">
-          <div class="footer-section">
-              <h3>Links Importantes</h3>
-              <ul>
-                  <li><a href="#">Sobre Nós</a></li>
-                  <li><a href="#">Serviços</a></li>
-                  <li><a href="#">Contato</a></li>
-              </ul>
-          </div>
+    useEffect(() => {
+		if (carrinho.length > 0) {
+			localStorage.setItem("carrinho", JSON.stringify(carrinho));
+		}
+	}, [carrinho]);
 
-          <div class="footer-section">
-              <div class="social-icons">
-                  <a href="https://wa.me/91983075446" title="WhatsApp">
-                      <img src="./src/assets/footer Whatsapp.jpg" alt="WhatsApp"/>
-                  </a>
-                  <a href="https://www.instagram.com/luiiskkj/" title="Instagram">
-                      <img src="./src/assets/instagram footer.jpg" alt="Instagram"/>
-                  </a>
-              </div>
-          </div>
-      </div>
+    const adicionarAoCarrinho = (produto) => {
+		const produtoExistente = carrinho.find((item) => item.id === produto.id);
 
-      <div class="footer-bottom">
-          &copy; 2024 MineDev. Feito por Luis.
-      </div>
-     </footer>
+		if (produtoExistente) {
+			const carrinhoAtualizado = carrinho.map((item) =>
+				item.id === produto.id
+					? { ...item, quantity: item.quantity + 1 }
+					: item,
+			);
+			setCarrinho(carrinhoAtualizado);
+		} else {
+			const carrinhoAtualizado = [...carrinho, { ...produto, quantity: 1 }];
+			setCarrinho(carrinhoAtualizado);
+		}
+	};
+	return (
+		<header>
+			<div className="navbar">
+				<div className="conteúdo interno do cabeçalho">
+					<h1 className="logo">
+						Mine <span>Dev</span>
+					</h1>
+					<nav>
+						<ul>
+							<li>
+								<a href="/">Sobre nosso site!</a>
+							</li>
+							<li>
+								<a href="/">Contato!</a>
+							</li>
+							<li>
+								<a href="/">Somos confiáveis!?</a>
+							</li>
+						</ul>
+					</nav>
+					<div className="nav-img-container">
+						<img src="/src/assets/cart.png" alt="IMAGEM DO CART!" />
+						<img src="/src/assets/menu.png" alt="IMAGEM DO MENU!" />
+					</div>
+				</div>
+			</div>
+			<div className="container">
+				{produtos.map((product) => (
+					<div key={product.id} className="card card1">
+						<img src={product.imgUrl} alt={product.name} />
+						<h1>{product.name}</h1>
+						<p>{product.descricao}</p>
+						<button type="button" className="btn-1" onClick={() => adicionarAoCarrinho(product)}>{`R$ ${product.price}`}</button>
+					</div>
+				))}
+			</div>
 
-      </header>
-    )
-  };
-  
+			<footer>
+				<div className="footer-container">
+					<div className="footer-section">
+						<h3>Links Importantes</h3>
+						<ul>
+							<li>
+								<a href="/">Sobre Nós</a>
+							</li>
+							<li>
+								<a href="/">Serviços</a>
+							</li>
+							<li>
+								<a href="/">Contato</a>
+							</li>
+						</ul>
+					</div>
+
+					<div className="footer-section">
+						<div className="social-icons">
+							<a href="https://wa.me/91983075446" title="WhatsApp">
+								<img src="/src/assets/footer Whatsapp.jpg" alt="WhatsApp" />
+							</a>
+							<a href="https://www.instagram.com/luiiskkj/" title="Instagram">
+								<img src="/src/assets/instagram footer.jpg" alt="Instagram" />
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div className="footer-bottom">&copy; 2024 MineDev. Feito por Luis.</div>
+			</footer>
+		</header>
+	);
+};
+
 export default Home;
-  
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,35 +1,86 @@
-import React from 'react'
-import './Login.css'
+import React, { useState } from "react";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+	const [email, setEmail] = useState("");
+	const [senha, setSenha] = useState("");
+	const [message, setMessage] = useState(null);
+	const [messageType, setMessageType] = useState("");
+	const navigate = useNavigate();
 
-  return (
+	async function handleLogin(e) {
+		e.preventDefault();
 
-    <div className='container-page-login'>
-      <div className='container-login'>
-        <form className=''>
-          <h1>Login</h1>
-          
-          <div className='input-box-login'>
-            <input type="email" placeholder='Email' required autoFocus/>
-            <i><box-icon type='solid' name='user'></box-icon></i>
-          </div>
-          <div className='input-box-login'>
-            <input type="password" placeholder='Senha' required/>
-            <i><box-icon type='solid' name='lock'></box-icon></i>
-          </div>
+		try {
+			const response = await axios.get("http://localhost:5000/clientes", {
+				params: { email, senha },
+			});
 
-          <button type='submit' className='btn'>Logar</button>
+			if (response.data.length > 0) {
+				const userData = response.data[0];
+				localStorage.setItem("client", JSON.stringify(userData));
 
-          <div className='login-link'>
-            <p>Não tem uma conta? <a href="/">Registrar</a></p>
-          </div>
-          
-        </form>
-    </div>
-    </div>
-  );
+				setMessage("Login efetuado com sucesso");
+				setMessageType("success");
+				setTimeout(() => navigate("/"), 2000);
 
+				setEmail("");
+				setSenha("");
+			} else {
+				setMessage("Email ou senha inválidos");
+				setMessageType("error");
+			}
+		} catch (error) {
+			console.error("Erro ao fazer login:", error);
+			setMessage("Erro ao fazer login");
+			setMessageType("error");
+		}
+	}
+	return (
+		<div className="container-page-login">
+			<div className="container-login">
+				<form onSubmit={handleLogin} className="">
+					<h1>Login</h1>
+
+					{message && (
+						<div
+							className={`${
+								messageType === "success"
+									? "bg-green-100 border-green-400 text-green-700"
+									: "bg-red-100 border-red-400 text-red-700"
+							} border p-3 rounded-md mb-3`}
+						>
+							{message}
+						</div>
+					)}
+					<div className="input-box-login">
+						<input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)}/>
+						<i>
+							<box-icon type="solid" name="user" />
+						</i>
+					</div>
+					<div className="input-box-login">
+						<input type="password" placeholder="Senha" required value={senha} onChange={(e) => setSenha(e.target.value)} />
+						<i>
+							<box-icon type="solid" name="lock" />
+						</i>
+					</div>
+
+					<button type="submit" className="btn">
+						Logar
+					</button>
+
+					<div className="login-link">
+						<p>
+							Não tem uma conta? <a href="/registro">Registrar</a>
+						</p>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
